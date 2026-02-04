24e53158b6c65c3a786687a71d0601ea1a1bdee6
Ana Gomes <anapgomes41@gmail.com>
2026-02-04 10:44:08 -0300

    Adiciona tarefa na tela

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,9 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task")
 const formAdicionarTarefa = document.querySelector(".app__form-add-task")
 const textarea = document.querySelector(".app__form-textarea")
+const ulTarefas = document.querySelector(".app__section-task-list")
 
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement("li")
@@ -21,14 +22,18 @@ function criarElementoTarefa(tarefa) {
     `
     const paragrafo = document.createElement("p")
     paragrafo.textContent = tarefa.descricao
-    
+    paragrafo.classList.add("app__section-task-list-item-description")
+
     const botao = document.createElement("button")
+    botao.classList.add("app_button-edit")
+
     const imagemBotao = document.createElement("img")
     imagemBotao.setAttribute("src", "./imagens/edit.png")
     botao.appendChild(imagemBotao)
     li.appendChild(svg)
     li.appendChild(paragrafo)
     li.appendChild(botao)
+    return li
 }
 
 btnAdicionarTarefa.addEventListener("click", () => {
@@ -41,6 +46,14 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
         descricao: textarea.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.appendChild(elementoTarefa)
+    textarea.value = ""
+    formAdicionarTarefa.classList.add("hidden")
+})
+
+tarefas.forEach((tarefa) => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
 })
 
